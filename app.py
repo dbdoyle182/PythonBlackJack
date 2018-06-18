ae1bf11347d60654817befdb8ec9436c2c7a475f
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-18 19:31:16 -0400

    tested card class and added magic print method

--- app.py
+++ app.py
@@ -18,4 +18,10 @@ class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+card = Card("Hearts", "Two")
+print(card)
     
