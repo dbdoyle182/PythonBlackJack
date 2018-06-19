4d780221a22c7feb5917626221a8f86756fe2efe
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-19 18:08:30 -0400

    almost done with game logic

--- app.py
+++ app.py
@@ -261,9 +261,22 @@ while True:
         show_some(player_hand, dealer_hand)
 
         # Bust conditional statement
-        if player_hand,value > 21:
+        if player_hand.value > 21:
             player_busts(player_hand, dealer_hand, player_chips)
             break
+        
+    # If player hasn't busted, play the same logic for the dealer
+    if player_hand.value <= 21:
+
+        while dealer_hand.value < 17:
+            hit(deck, dealer_hand)
+
+        # Show all the cards
+        show_all(player_hand, dealer_hand)
+
+        # Test different winning scenarios if neither player nor dealer have busted yet
+        
+
 
 
 
