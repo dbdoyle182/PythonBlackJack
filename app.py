16f7d92665211016425938d3363e75c5585db595
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-18 22:10:23 -0400

    added take bet function

--- app.py
+++ app.py
@@ -112,6 +112,22 @@ class Chips:
         self.total -= self.bet
 
 
+# Function for taking in user bets
+def take_bet(chips):
+    while True:
+        try:
+            chips.bet = int(input("How many chips would you like to bet?"))
+        except ValueError:
+            print("Sorry, bet must be an integer!")
+        else:
+            if chips.bet > chips.total:
+                print(f"Bet cannot exceed {chips.total}")
+            else:
+                break
+
+
+
+
 
 
 
